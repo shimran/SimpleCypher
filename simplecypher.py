e7b04c3b29dffab8217d147c2ff4c060e9aa864c
Shimran George <shimran.george@jivesoftware.com>
2016-11-16 13:38:43 -0800

    Add dynamic user input for message, and encryption  flag

--- simplecypher.py
+++ simplecypher.py
@@ -8,4 +8,6 @@ Read me for more details, but goal is to take a message and encrypt/decrypt base
 #Will need to add a lower() function to induce case-sensitivity
 Alphabet_Key= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+message = input("Enter phrase to be encrypted: ")
+flag = input("Do you want to set the ENCRYPT (E) or DECRYPT (D)?")
 
