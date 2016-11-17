8ffb0f00c8b1823011d5f11dedfad5da6c648201
Shimran George <shimran.george@jivesoftware.com>
2016-11-17 10:45:09 -0800

    Add main method, shells for encrypt/decrypt

--- simplecypher.py
+++ simplecypher.py
@@ -8,12 +8,18 @@ Read me for more details, but goal is to take a message and encrypt/decrypt base
 
 import sys,math, string
 
-#Add akphabet for the Cypher shift
+#Define main
+def main():
+
+#Add alphabet for the Cypher shift
 #Will need to add a upper() function to induce case-sensitivity
 alphabetList= list(string.ascii_uppercase)
 shiftKey= input("Add Key for shift (must be 26 letters):")
 message = input("Enter phrase to be encrypted: ")
 flag = input("Do you want to set the ENCRYPT (E) or DECRYPT (D)?:")
+
+
+
 
 #Check if shift Key is valid, sort to see if list is the same length
 def checkValidShiftKey(shiftKey,alphabetList):
@@ -21,5 +27,15 @@ def checkValidShiftKey(shiftKey,alphabetList):
 	if len(alphabetList) != len(keySort)
 	'The key you have provided is invalid. Check length or if a letter not part of the alphabet'
 	sys.exit('The key you have provided is invalid. Check length or if a letter not part of the alphabet')
+
+
+#Function for encryption
+
+#Function for decryption
+
+#Allows function to be imported as module
+if __name__ == '__main__':
+main()
+
 
 	
